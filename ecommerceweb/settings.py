c5257ffff61a26dbf8fd36d10c19d0a965244bfc
Sam <samtisawesomest@gmail.com>
2020-02-26 23:33:18 +0800

    Turned debuging off

--- ecommerceweb/settings.py
+++ ecommerceweb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@b4itwq1bcx#y@1+u-pemchcvy$w)p=ga#2urcuai-@+i^%2qq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
